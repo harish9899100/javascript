11ce7f592f77255f02d654168631de94444e7e00
harish khileri <harishkhilery98@gmail.com>
2025-03-28 12:34:59 +0530

    add new file name is practice_javascript.js

---
+++ practice_javascript.js
@@ -0,0 +1,6 @@
+// function changeMessage() {
+//     document.getElementById('demo').innerText = 'The message has been changed!';
+// }
+function change() {
+    document.getElementById("message").innerText = "The message value has been changed..";
+}
