a1b9a82c008675cf12d62d09155ffced640b78ee
harish khileri <harishkhilery98@gmail.com>
2025-03-28 12:42:42 +0530

    deleted demo.js

--- demo.js
+++
@@ -1,7 +0,0 @@
-let x = "school";
-let y = "college";
-let z = x + y;
-console.log("the value of x and y is ::",z);
-console.log("location");
-let name = prompt("Please enter your name:");
-console.log("Hello, " + name + "!");
--- practice_javascript.js
+++
@@ -1,6 +0,0 @@
-// function changeMessage() {
-//     document.getElementById('demo').innerText = 'The message has been changed!';
-// }
-function change() {
-    document.getElementById("message").innerText = "The message value has been changed..";
-}
