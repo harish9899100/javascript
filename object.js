eac5602ddf4a0aab52d7cd8a25035ae03928d3a5
harish khileri <harishkhilery98@gmail.com>
2025-04-02 14:26:51 +0530

    practice of objects

---
+++ object.js
@@ -0,0 +1,52 @@
+// function currentdate(){
+//     const today = new Date()
+//     return today;
+// }
+// console.log(currentdate());
+
+// let person = {
+//     name:"dev",
+//     age:"25",
+//     id:"12345",
+//     rollno:"581618"
+// }
+// console.log(" Name of person is:" + person.name + "\n age of person is: " + person.age + "\n id of person is:" + person.id + "\n roll number of person is:" + person.rollno);
+
+
+
+// const person = {
+//     fisrtname:"Rohit",
+//     secoundname:"Choudhary",
+//     age:"19",
+//     Rollno:"581620"
+// }
+// let a = "fisrtname";
+// let b = "secoundname";
+// let c = "age";
+// console.log(person[a] + " " + person[b] + " is " + person[c] + " year old " );
+
+
+
+// const person ={
+//     name:"rakesh",
+//     age:"22",
+//     rollno:"35132543"
+// }
+// console.log(person["name"] + " is " + person["age"] + " years old");
+
+
+// const person ={
+//     fname:"rakesh",
+//     sname:"kumar",
+//     age:"22",
+//     rollno:"3453245345",
+//     fullname: function(){
+//        return this.fname + " " + this.sname;
+//     }
+
+// };
+// console.log(person.fullname());
+
+
+
+
