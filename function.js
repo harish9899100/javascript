1f9ba61dafcd6a9eb01f22e66a130b3bc93941d1
harish khileri <harishkhilery98@gmail.com>
2025-04-02 15:04:34 +0530

    function in js

---
+++ function.js
@@ -0,0 +1,24 @@
+console.log("practice of function in javascript:");
+function add(a, b){
+    return a + b;
+}
+let a = add(50, 50);
+console.log(a);
+
+function sub(a, b){
+    return a - b;
+}
+let b = sub(50,30)
+console.log(b);
+
+function mult(a, b){
+    return a * b;
+}
+let c = mult(50,50);
+console.log(c);
+
+function divi(a, b){
+    return a / b;
+}
+let d = divi(20, 2);
+console.log(d);
