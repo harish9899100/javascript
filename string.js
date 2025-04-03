ae9de0af0cbef9942265d861f7618300d1b09220
harish khileri <harishkhilery98@gmail.com>
2025-04-03 14:28:04 +0530

    how to write string

---
+++ string.js
@@ -0,0 +1,5 @@
+let x = "this is a simple string usign dubble quots";
+let y = 'this is a simple string using single quots';
+let z = 'how to write string inside "string"';
+let a = "what's going everyone";
+console.log(x + "\n" + y + "\n" + z + "\n" + a);
