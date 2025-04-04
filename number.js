b7eaafac2e6c1f560dee0eb53e98282f83c5d353
harish khileri <harishkhilery98@gmail.com>
2025-04-04 16:52:40 +0530

    number practice

---
+++ number.js
@@ -0,0 +1,36 @@
+// using this file for practice of all numbers methods ...
+// let x = 15;
+// let y = 6;
+// let z = x + y;      //21
+// console.log(z);
+
+
+// let x = 5 + 10;
+// let y = (2*5 + 50) / 10;
+// let z = x + y ;
+// console.log("5 + 10=",x);
+// console.log("2*5 + 10=",y);
+// console.log("x + y=",z); 
+
+
+// NaN this means illigl number
+// let x = 15;
+// let y = "Sir";
+// let z0 = x + y;   //concneting 
+// let z1 = x - y;   //NaN
+// let z2 = x * y;
+// let z3 = x / y;
+// console.log(z0);
+// console.log(z1);
+// console.log(z2);
+// console.log(z3); 
+
+//isNaN
+// let x = 23 / "sir";
+// console.log(isNaN(x));  //true
+// let y = 2 + 4;
+// console.log(isNaN(y))   //false  
+
+//typeof NaN
+let x = NaN;
+console.log(typeof(x));
