4862c6eaa70faf1f98be475ba205b1f6f7facd15
harish khileri <harishkhilery98@gmail.com>
2025-04-07 13:55:08 +0530

    practice all about numbers

--- number.js
+++ number.js
@@ -32,5 +32,87 @@
 // console.log(isNaN(y))   //false  
 
 //typeof NaN
-let x = NaN;
-console.log(typeof(x));
+// let x = NaN;
+// console.log(typeof(x));    //number
+
+
+//slice method
+// let x = "hello everyone how are you";
+// let t = x.toUpperCase();
+// console.log(t);
+// let b = "HELLO EVERYONE WHERE ARE FROM";
+// let z = b.toLowerCase();
+// console.log(z)
+// let c = "hello sir\n";
+// let d = c.repeat(6);
+// console.log(d);
+
+
+// let x = Number.MAX_SAFE_INTEGER;
+// console.log(x);
+// let y = Number.MIN_SAFE_INTEGER;
+// console.log(y);
+
+
+// let x = 222;
+// y = x.toString;
+// console.log("The value of y is :" + y);
+
+
+// let x = 222;
+// console.log(x.toExponential());
+// console.log(x.toExponential(1));
+
+// let x = 222;
+// console.log(x.toFixed(2));
+
+
+//  let x = Date();
+//  console.log(x);
+
+// number () convert date to number
+// let x = new Date("01-01-2025");
+// let y = new Date("02-02-2025");
+// console.log(Number(x));
+// console.log(Number(y));
+
+//parsInt() 
+// console.log(parseInt("  10 34 76"));
+// console.log(parseInt("rohit"));
+// console.log(parseFloat("23"));
+// console.log(parseFloat("  87 87 98"));
+
+// console.log(Number.isInteger(78));
+// console.log(Number.isInteger("45"));
+// console.log(Number.isInteger(45.45));
+
+
+// console.log(Number.isInteger(15));  //t
+// console.log(Number.isFinite(45645756));   //t
+//console.log(Number.isSafeInteger(2353463463950460687867));  //f
+
+
+// let x = Number.EPSILON;
+// console.log(x);
+
+//  let x = Number.MAX_VALUE;
+//  console.log("MIX_VALUE:" + x);
+//  let y = Number.MIN_VALUE;
+//  console.log("MIN_VALUE:" + y);
+
+// let person = {
+//     fname:"rohit",
+//     lname:"kumar",
+//     id:"studnet",
+//     rolln:546456,
+//     fullname:function(){
+//         return this.fname + " " + this.lname;    }
+// }
+// console.log(person);
+
+
+
+// let x = 5.45;
+// console.log(x.toExponential(3));
+// let y = 15;
+// console.log(x.toFixed(5))
