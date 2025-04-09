452f34ac2371ccdeece605174761829f57429bba
harish khileri <harishkhilery98@gmail.com>
2025-04-09 18:48:16 +0530

    practice of Math.method()

---
+++ math.js
@@ -0,0 +1,61 @@
+// value of PI in js
+// console.log("The value of PI: " + Math.PI);
+// console.log("The value of Math.E: " + Math.E);
+// console.log("The value of Math.LN10: " + Math.LN10);
+// console.log("The value of Math.LN2: " + Math.LN2);
+// console.log("The value of Math.LOG10E: " + Math.LOG10E);
+// console.log("The value of Math.LOG2E: " + Math.LOG2E);
+// console.log("The value of Math.SQRT1_2: " + Math.SQRT1_2);
+// console.log("The value of Math.SQRT2" + Math.SQRT2);
+// console.log("$$THANK YOU$$");
+
+//Math.round()  return nearest element 
+// console.log(Math.round(8.8));
+// console.log(Math.round(8.5));
+// console.log(Math.round(8.4));
+
+//Math.ceil()   return uper nearest element
+// console.log(Math.ceil(4.2));
+// console.log(Math.ceil(4.8));
+// console.log(Math.ceil(-4.2));
+
+
+//Math.floor()   return down to it's nearest element
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.1));
+// console.log(Math.floor(-4.9));
+// console.log(Math.floor(-4.1));
+ 
+
+//Math.trunch()  return the integer 
+// console.log(Math.trunc(4.9));
+// console.log(Math.trunc(4.1));
+// console.log(Math.trunc(-4.9));
+
+//Math.sign() if number is possitive:1, number is nagetive:-1, number is zero:0
+// console.log(Math.sign(115));
+// console.log(Math.sign(-20));
+// console.log(Math.sign(0));
+
+//Math.pow() return the power of element
+// console.log(Math.pow(2,2));  //4
+
+//Math.sqrt return the root of square
+// console.log(Math.sqrt(64));
+
+//Math.abs()  retrun absilute possitive value
+//console.log(Math.abs(-3.9));
+
+//Math.Min() ,Max()
+// console.log(Math.min(2,23,34,45,56,67,-15));
+// console.log(Math.max(2,34,345,456,57,687,9,-57));
+
+//Math.random()   writing any random number between o and 1
+// console.log(Math.random());
+// console.log(Math.random());
+
+//Math.log(),log2(), log10()
+// console.log(Math.log(1000));
+// console.log(Math.log2(1000));
+// console.log(Math.log10(1000));
+
