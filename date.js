3f561d7ce9cabefb7399d3b773b99f640933dcb2
harish khileri <harishkhilery98@gmail.com>
2025-04-09 17:41:08 +0530

    practice of date in js

---
+++ date.js
@@ -0,0 +1,61 @@
+//Date
+// let a = Date();
+// console.log(a);
+
+//new Date
+// let b = new Date("01-02-2024");
+// console.log(b);
+
+// let a = new Date("2018");
+// console.log("The six degit of date means year, month, date, time, hour, menuit:" + a);
+
+// //toString
+// let a = new Date();
+// let b = a.toString();
+// console.log(b);
+
+//toDateString()
+// let a = new Date();
+// let b = a.toDateString();
+// console.log(b);
+
+//toUTCString()
+// let a = new Date();
+// console.log(a.toUTCString());
+
+//toISOString()
+// let a = new Date();
+// console.log(a.toISOString());
+
+// let a = new Date("2025-03-15");
+// console.log(a.toString());
+// let b = new Date("04/03/2024");
+// console.log(b.toString());
+
+
+// let a = new Date("january 15 2024");
+// console.log(a);
+
+// Date.parse()  microseconds
+// let a = Date.parse("january 15 2025");
+// console.log(a);
+
+// let a = new Date();
+// console.log(a.getFullYear());
+// console.log(a.getDate());
+// console.log(a.getDay());
+// console.log(a.getHours());
+// console.log(a.getMilliseconds());
+// console.log(a.getMinutes());
+// console.log(a.getTimezoneOffset());
+// console.log(a.getUTCDate());
+
+//setmethod()
+let a = new Date("january 01, 2025");
+//console.log(a.setDate(26));
+// console.log(a.setFullYear(2024, 21, 11));
+console.log(a.setHours(22, 10, 20));
+// console.log(a.setMilliseconds(28));
+// console.log(a.setTime(34));
+// console.log(a.setUTCMilliseconds(27656746));
+
