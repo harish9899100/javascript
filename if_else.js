8d6757cc99c7dc43538cd709ad123aca1bf49de3
harish khileri <harishkhilery98@gmail.com>
2025-04-10 10:49:21 +0530

    how to use simple if else statement

---
+++ if_else.js
@@ -0,0 +1,34 @@
+//if:
+// let a = 34;
+// let b = 2;
+// if(a>b){
+//     console.log("a is grater than b");
+// }
+
+//if else:
+let age = 21;
+if (age > 18){
+    console.log("Your age is: " + age + "\nYou can vote:");
+}
+else{
+console.log("You can't vote:")
+}
+
+//else if:
+// let a = 2;
+// let b = 4;
+// let c = 6;
+// let d = 8;
+// if(a>b){
+//     console.log("b is grater than a:");
+// }
+// else if(b>c){
+//     console.log("c is grater than b");
+// }
+// else if(c>d){
+//     console.log("c is grater than d");
+// }
+// else{
+//     console.log("if statement's all conditations are wrong:: ");
+// }
+
