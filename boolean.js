ec303c77a3c0a93a22f60894bd6c48a7f923e670
harish khileri <harishkhilery98@gmail.com>
2025-04-10 09:15:05 +0530

    what is Boolean()

---
+++ boolean.js
@@ -0,0 +1,30 @@
+// console.log(5 > 3);
+// console.log(2.2 > 2);
+// console.log(1.1 > 1);
+// console.log(-4 > -5);
+// console.log(8 === 8.0);
+// console.log(8 < 7);
+
+// console.log(Boolean(100));
+// console.log(Boolean(0));
+// console.log(Boolean("hello"));
+// console.log(Boolean(true));
+// console.log (Boolean(1 + 2 +7));
+// console.log(Boolean(-14));
+
+// NaN is false
+// let a = 10 * "hii";
+// console.log(Boolean(a));
+
+// let x = Boolean(false);
+// let y = new Boolean(false);
+// console.log(x == y);
+
+// let a = Boolean(true);
+// let b = Boolean(true);
+// console.log(a === b);
+
+// let a = Boolean(false);
+// let b = Boolean(true);
+// console.log(a === b);
+//console.log(Boolean(NaN));
