2efb756239f4d06c745510472c74a1a83bee67e9
harish khileri <harishkhilery98@gmail.com>
2025-04-10 17:10:55 +0530

    loops and set

---
+++ loop.js
@@ -0,0 +1,89 @@
+// const car = ['BMW', 'AUDY', 'RUBICORN', 'BE6', 'THAR', 'SCORPIO', 'TOYOTA'];
+// for(a = 0;a < car.length;a++){
+//     console.log(car[a]);
+// }
+
+// for (a=1;a<=10;a++){
+//     console.log(a);
+// }
+
+// for(a=1;a<=10;a++){
+//     console.log("The table of number is :" + a * 8);
+// }
+
+// let person = {name:"rohit", age:"23", rollno:"34534"};
+// for(let x in person){
+//     console.log(person[x]);
+// }
+
+// let cars = ["BMW", "AUDY", "BE7"];
+// for(let x of cars){
+//     console.log(x);
+// }
+
+// //while loop
+// let a = 1;
+// while(a < 5){
+//     console.log("hey");
+//     a++
+// }
+
+
+// let i = 0;
+// do{
+//     console.log("hello");
+//     i++;
+// }
+// while(i < 2)
+
+// let fruits = ["apple", "mango", "papaya", "kiwi"];
+// for(let fruit of fruits){
+//     console.log(fruit);
+// }
+
+
+// let cars = {name:"bmw",color:"blue", id:"34566"};
+// for(let x in cars){
+//     console.log(cars[x]);
+// }
+
+// let a;
+// for (a = 1;a<=10;a++){
+//     if(a === 3){break}
+//         console.log("print the value of a is:" + a);
+//     }
+
+// let a;
+// for(a=1;a<11;a++){
+//     if(a===4){continue}
+//     console.log("the value of a is:" + a);
+// }
+
+
+// let cars = ["bmw", "thar", "scorpio", "be6"];
+// list:{
+//    console.log(cars[0]);
+//     console.log(cars[1]);
+//     break list;
+//     console.log(cars[2]);
+//     console.log(cars[3]);
+// }
+
+
+// let a = "hello everyone";
+// for(const b of a){
+//     console.log(b);
+// }
+
+// let a = [1,2,3,4,5,6,7,8,9,0];
+// for(const b of a){
+//     console.log(b);
+// }
+
+// let a = new Set(['1', '2', '0', '8']);
+//     console.log(a.size);
+
+let word = new Set(['harish', 'khileri']);
+word.add(['village']);
+word.add(['us']);
+console.log(word.size);
