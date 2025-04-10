502e7882b1e0cc137044303eed97c01b9af4a63a
harish khileri <harishkhilery98@gmail.com>
2025-04-10 12:21:41 +0530

    switch statement

---
+++ switch.js
@@ -0,0 +1,67 @@
+// let day;
+// switch(new Date().getDay()){
+//     case 0:
+//         day = "sunday";
+//         break;
+//     case 1:
+//         day = "monday";
+//         break;
+//     case 2:
+//         day = "tuesday";
+//         break;
+//     case 3:
+//         day = "wednesday";
+//         break;
+//     case 4:
+//         day = "thursday";
+//         break;
+//     case 5:
+//         day = "friday";
+//         break;
+//     case 6:
+//         day = "saturday";
+// }
+// console.log("The day of today is :" + day);
+
+// let day;
+// switch(new Date().getDay()){
+//     case 6:
+//         day = "saturday";
+//         break;
+//     case 0:
+//         day = "sanday";
+//         break;
+//     default:
+//         console.log("check your day properly");
+// }
+
+
+// let fruits;
+// switch(fruits = "mango"){
+//     case "banana":
+//         console.log("This is a banana");
+//         break;
+//     case "apple":
+//         console.log("This is a apple");
+//         break;
+//     case "kiwi":
+//         console.log("This is a kiwi");
+//         break;
+//     case "mango":
+//         console.log("This is a mango");
+//         break;
+//     default:
+//         console.log("it's not fruit:")
+//}
+
+let num = 400;
+switch(num % 2){
+    case 0:
+        console.log("Number is even");
+        break; 
+    case 1:
+        console.log("Number is odd");
+        break;
+    default:
+        console.log("This is invelit number");
+}
